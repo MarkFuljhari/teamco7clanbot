888b54d82ed1cd66c8fb5d7c6c6529f36e78407b
MarkFuljhari <45743552+MarkFuljhari@users.noreply.github.com>
2019-06-03 21:34:12 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,20 +1,44 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-var prefix = "!"
+var prefix = "!zg "
+
+//// CONSOLE REPLY & GAME ACTIVITY STATUS OF THE BOT: ////
 
 client.on('ready', () => {
-  console.log('I am ready!');
-  console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`);
-  client.user.setActivity(`!help | ${client.users.size} servers`);
-});
+    console.log(`Logged in as ${client.user.tag}!`);
+    console.log('Success! I am online on DiscordApp.');
+    console.log(`Bot has woken up! With ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} servers.`);
+    client.user.setActivity(`!help | ${client.users.size} users`, { type: 'PLAYING' })
+    .then(presence => console.log(`Activity set to ${presence.game ? presence.game.name : `!help`}`))
+    .catch(console.error);
+  });
 
-client.on('message', async msg => {
-    const args = msg.content.slice(prefix.length).trim().split(/ +/g);
-    var argresult = args.join(' ');
+client.on('message', message => {
+  
+         //// INFORMATION COMMANDS: /////
+  
+  	// PING CMD
+     if (msg.content.startsWith(prefix + 'ping')) {
+    //   const t = await msg.channel.send("Ping ?");
+    //   t.edit(`Pong! Latency is **${t.createdTimestamp - msg.createdTimestamp}**ms. API Latency is **${Math.round(client.ping)}**ms`); DON'T DELETE THIS EITHER
+    let msgping1 = new Date();
+    let botping = new Date() - msg.createdAt;
+    let msgping2 = new Date() - msgping1;
+ 
+    let pingembed = new Discord.RichEmbed()
+        .setColor("RANDOM")
+        .addField('Discord API Ping: ', Math.floor(botping) + 'ms')
+        .addField('Bot Ping: ', Math.floor(botping) + 'ms')
+        .addField('Message Ping: ', '~' + Math.round(msgping2) + 'ms')
+        .setTimestamp(new Date())
+        .setFooter(`Requested by ${msg.author.tag}`);
+ 
+   return msg.channel.send(pingembed);
+   }
 
-//// ENTERTAINMENT COMMANDS ////
-
+         //// ENTERTAINMENT COMMANDS: ////
+ 
 // POKE CMD
 if (msg.content.startsWith(prefix + 'poke')){
 let member = msg.mentions.members.first();
@@ -31,5 +55,9 @@ if (msg.content.startsWith(prefix + 'wave')){
 let member = msg.mentions.members.first();
 msg.channel.send(`${member} , ${msg.author} waved at you.`);
 }
- 
+
+});
+//
 client.login(process.env.BOT_TOKEN);
+
+
