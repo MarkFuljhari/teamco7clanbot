53d65623334e2ff43927252bbfc5262b48d49570
MarkFuljhari <45743552+MarkFuljhari@users.noreply.github.com>
2019-06-03 22:04:38 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -15,43 +15,67 @@ client.on('message', async msg => {
 
     if(msg.author.bot) return; //This here means that if message was sent from the other bots EXEPT Akelli, our bot (Akelli) won't sent anything in the chat (thats why return there is e.g return, returns you to somewhere or something) this prevents bot-ception (when multiple bots were to use the same prefix).
 
-  // HELP COMMAND (EMBED MESSAGE: https://embedbuilder.nadekobot.me/)
-  	if (msg.content.startsWith(prefix + 'help')) {
-      	msg.channel.send({embed: {
-  "plainText": "",
-  "title": "Commands",
-  "color": 3447003,
-  "footer": {
-    "text": " Copyright @teamco7clan",
-    "icon_url": "https://cdn.discordapp.com/avatars/481540507479048203/6aaed6c31705efac5eb4906a9605ac14.png?size=2048"
-  },
-  "thumbnail": "",
-  "fields": [
-    {
-      "name": "Music",
-      "value": "Developer required for JavaScript.",
-      "inline": false
-    },
-    {
-      "name": "Entertainmemt",
-      "value": "Developing",
-      "inline": false
-    },
-    {
-      "name": "Information",
-      "value": "Developing",
-      "inline": false
-    },
-    {
-      "name": "Staff Administration",
-      "value": "Developing",
-      "inline": false
-    }
-  ]
-}
-  });
-}
-
+  
+ // HELP CMD (EMBED)
+  if (msg.content.startsWith(prefix + 'help')) {
+    msg.channel.send({embed:{
+      "title": "",
+      "description": "Hi! I'm TeamCo7Bot!\nI am developed by TeamCo7Clan & @akellicompany.\I contain entertaining & reliable information commands.\n\nFor a list of commands send **!commands**\n\nJoin our Discord!\nhttps://discord.me/teamco7clan",
+      "color": 3447003,
+      "footer": {
+      "text": "© Copyright TeamCo7Clan"
  
+      },
+      "author": {
+      "name": "TeamCo7Clan Bot Introduction",
+      "url": "https://thebestgameryt.github.io/AkelliBot/",
+      "icon_url": "https://cdn.discordapp.com/avatars/534003592542027786/10199bbc68d2c69dea28e76772e175ba.png?size=2048"
+      }
+      }
+      });
+      }
+ 
+   // COMMANDS CMD (EMBED)
+   if (msg.content.startsWith(prefix + 'commands')) {
+    msg.member.send({embed:
+      {
+      "title": "Default prefix: !",
+      "description": "Need help? Join our Discord server.",
+      "author": {
+        "name": "Commands",
+        "icon_url": "https://cdn.discordapp.com/avatars/534003592542027786/10199bbc68d2c69dea28e76772e175ba.png?size=2048"
+      },
+      "color": 553380,
+      "footer": {
+        "text": "© Copyright TeamCo7Clan, @teamco7clan"
+      },
+      "fields": [
+        {
+          "name": "» Information",
+          "value": "!ping, !myavatar, !avatar, !stats, !serverinfo, !credits, !socialmedia, !invite",
+          "inline": false
+        },
+        {
+          "name": "» Entertainment",
+          "value": "!poke, !hi, !weed, !haveibeenpwned, !dog, !cat, !meme, !boob, !slots, !8ball",
+          "inline": false
+        },
+        {
+          "name": "» Staff Administration",
+          "value": "!ban, !kick, !report, !poll, !purge, !mute, !warn, !setgame, !setprefix",
+          "inline": false
+        },
+        {
+          "name": "» Music",
+          "value": "Currently unavailable. Software Developer required!",
+          "inline": false
+        }
+      ]
+    }
+  });
+msg.reply('check your inbox for your commands!\nBot is still under development, not all commands shall work!');
+   }
+ 
+
 });
 client.login(process.env.BOT_TOKEN);
