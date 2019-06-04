8bf65b15b07199f0e334b2905868105ee3e8a070
MarkFuljhari <45743552+MarkFuljhari@users.noreply.github.com>
2019-06-03 21:59:25 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,69 +3,55 @@ const client = new Discord.Client();
 
 var prefix = "!"
 
-//// SHORTCUT TEXTS IN THE JAVASCRIPT (CODE): ////
- 
-client.on('message', async msg => {
-  const args = msg.content.slice(prefix.length).trim().split(/ +/g);
- 
-  if(msg.author.bot) return;
- 
-  if(msg.content.indexOf(prefix) !== 0) return;
-
-//// CONSOLE REPLY & GAME ACTIVITY STATUS OF THE BOT: ////
-
 client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
-    console.log('Success! I am online on DiscordApp.');
-    console.log(`Bot has woken up! With ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} servers.`);
-    client.user.setActivity(`!help | ${client.users.size} users`, { type: 'PLAYING' })
-    .then(presence => console.log(`Activity set to ${presence.game ? presence.game.name : `!help`}`))
-    .catch(console.error);
-  });
-
-client.on('message', message => {
-  
-         //// INFORMATION COMMANDS: /////
-  
-  	 
-    // PING CMD
-     if (msg.content.startsWith(prefix + 'ping')) {
-    //   const t = await msg.channel.send("Ping ?");
-    //   t.edit(`Pong! Latency is **${t.createdTimestamp - msg.createdTimestamp}**ms. API Latency is **${Math.round(client.ping)}**ms`); DON'T DELETE THIS EITHER
-    let msgping1 = new Date();
-    let botping = new Date() - msg.createdAt;
-    let msgping2 = new Date() - msgping1;
- 
-    let pingembed = new Discord.RichEmbed()
-        .setColor("RANDOM")
-        .addField('Discord API Ping: ', Math.floor(botping) + 'ms')
-        .addField('Bot Ping: ', Math.floor(botping) + 'ms')
-        .addField('Message Ping: ', '~' + Math.round(msgping2) + 'ms')
-        .setTimestamp(new Date())
-        .setFooter(`Requested by ${msg.author.tag}`);
- 
-   return msg.channel.send(pingembed);
-   }
-
-         //// ENTERTAINMENT COMMANDS: ////
- 
-// POKE CMD
-if (msg.content.startsWith(prefix + 'poke')){
-let member = msg.mentions.members.first();
-msg.channel.send(`${member} got poked by ${msg.author}`);
-}
- 
-// HI CMD
-if (msg.content.startsWith(prefix + 'hi')){
-msg.channel.send('Hey wassup?');
-}
- 
-// HAND WAVE CMD
-if (msg.content.startsWith(prefix + 'wave')){
-let member = msg.mentions.members.first();
-msg.channel.send(`${member} , ${msg.author} waved at you.`);
-}
-
+  console.log('I am ready!');
+  console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`);
+  client.user.setActivity(`!help | ${client.users.size} servers`);
 });
-//
+
+client.on('message', async msg => {
+    const args = msg.content.slice(prefix.length).trim().split(/ +/g);
+    var argresult = args.join(' ');
+
+    if(msg.author.bot) return; //This here means that if message was sent from the other bots EXEPT Akelli, our bot (Akelli) won't sent anything in the chat (thats why return there is e.g return, returns you to somewhere or something) this prevents bot-ception (when multiple bots were to use the same prefix).
+
+  // HELP COMMAND (EMBED MESSAGE: https://embedbuilder.nadekobot.me/)
+  	if (msg.content.startsWith(prefix + 'help')) {
+      	msg.channel.send({embed: {
+  "plainText": "",
+  "title": "Commands",
+  "color": 3447003,
+  "footer": {
+    "text": " Copyright @teamco7clan",
+    "icon_url": "https://cdn.discordapp.com/avatars/481540507479048203/6aaed6c31705efac5eb4906a9605ac14.png?size=2048"
+  },
+  "thumbnail": "",
+  "fields": [
+    {
+      "name": "Music",
+      "value": "Developer required for JavaScript.",
+      "inline": false
+    },
+    {
+      "name": "Entertainmemt",
+      "value": "Developing",
+      "inline": false
+    },
+    {
+      "name": "Information",
+      "value": "Developing",
+      "inline": false
+    },
+    {
+      "name": "Staff Administration",
+      "value": "Developing",
+      "inline": false
+    }
+  ]
+}
+  });
+}
+
+ 
+});
 client.login(process.env.BOT_TOKEN);
