3494ae5359aac92955c9ae18ef03b114d814ffab
MarkFuljhari <45743552+MarkFuljhari@users.noreply.github.com>
2019-06-16 13:13:09 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -322,6 +322,21 @@ await user.ban(reason)
        banlog.send(banembed)
 }
  
+  // PURGE <AMMOUNT> COMMAND:
+if (msg.content.startsWith(prefix + 'purge')){
+    if (!msg.guild.member(client.user).hasPermission('MANAGE_MESSAGES')) return msg.channel.send(':no_entry `I do not have the correct permissions.`').catch(console.error);
+    if (!msg.member.hasPermission("MANAGE_MESSAGES")) return msg.channel.send(":no_entry: `Sorry, but you do not have valid permissions! If you beleive this is a error, contact an owner.`");
+    if (isNaN(args[1])) return msg.channel.send(':warning: `Please supply a valid amount of messages to purge`');
+    if (args[1] > 100) return msg.channel.send(':warning: `Please supply a number less than 100`');
+            msg.channel.bulkDelete(args[1]);
+            var cleanEmbed = new Discord.RichEmbed()
+            .setAuthor('Akelli Channel Cleaning')
+            .setDescription(`Cleaned **${args[1]}** messages :white_check_mark:`)
+            .setFooter('Requested by ' + msg.author.tag, msg.author.avatarURL)
+            .setColor('#ffffff');
+            msg.channel.send(cleanEmbed);
+}
+  
   // SET GAME ACTIVITY STATUS OF THE BOT MANUALLY (WITHOUT ENTERING SCRIPT/CODE)
 // SETGAME <TEXT>
 if (msg.content.startsWith(prefix + 'setgame')) {
